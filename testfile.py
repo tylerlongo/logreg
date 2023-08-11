54129fdbadc1758da6d4164917461013c3655974
tylerlongo <tylerjlongo9@gmail.com>
2023-08-11 18:35:42 -0400

    processing data from url

--- testfile.py
+++ testfile.py
@@ -1,12 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
+
+# Initialize a 2D list to store the table data
+finishes = []
+
+
 # URL of the webpage containing the table
 
-num = 1
-yr = 2023
+num = 48
+yr = [2022, 2023]
 
-url = "https://www.driveraverages.com/nascar/numberyear.php?carno_id=" + str(num) + "&yr_id=2023"
+url = "https://www.driveraverages.com/nascar/numberyear.php?carno_id=" + str(num) + "&yr_id=" + str(yr)
 
 # Send an HTTP GET request to the URL
 response = requests.get(url)
@@ -16,14 +21,20 @@ soup = BeautifulSoup(response.content, "html.parser")
 # Find the table containing the data
 table = soup.find("table")
 
-# Initialize a 2D list to store the table data
-finishes = []
-
 # Loop through each row in the table
+rownum = 0
 for row in table.find_all("tr"):
-    if len(row) >= 6:
-        cell = row.find_all(["th", "td"])[6]
-        finishes.append(cell.get_text(strip=True))
+    if rownum < 5:
+        rownum += 1
+    else:
+        cell = row.find_all(["th", "td"])[3]
+        if not cell.get_text(strip=True) == "":
+            week = []
+            cell = row.find_all(["th", "td"])[7]
+            week.append(int(cell.get_text(strip=True)))
+            cell = row.find_all(["th", "td"])[6]
+            week.append(int(cell.get_text(strip=True)))
+            finishes.append(week)
 
 # Print the 2D list
 print(finishes)

765c9551d8afe8c8b3d2302af16f50de66e318eb
Tyler Longo <tylerjlongo9@gmail.com>
2023-11-04 19:48:49 -0400

    updated output graph

--- main.py
+++ main.py
@@ -4,44 +4,76 @@ import numpy as np
 from logreg import LogisticRegression
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
+from PIL import Image
 
-nums = ((1, 1),
-        (2, 2),
-        (3, 3),
-        (4, 4),
-        (5, 5),
-        (6, 6),
-        (7, 7),
-        (8, 8),
-        (9, 9),
-        (10, 10),
-        (11, 11),
-        (12, 12),
-        (14, 14),
-        (15, 15),
-        (16, 16),
-        (17, 17),
-        (19, 19),
-        (20, 20),
-        (21, 21),
-        (22, 22),
-        (23, 23),
-        (24, 24),
-        (31, 31),
-        (34, 34),
-        (38, 38),
-        (41, 41),
-        (42, 42),
-        (43, 43),
-        (45, 45),
-        (47, 47),
-        (48, 48),
-        (51, 51),
-        (18, 54),
-        (77, 77),
-        (78, 78),
-        (99, 99))
-yrs = tuple(yr for yr in range(2022, 2024))
+nums = ((2021, 2, 2),
+        (2021, 3, 3),
+        (2021, 4, 4),
+        (2021, 5, 5),
+        (2021, 6, 6),
+        (2021, 7, 7),
+        (2021, 8, 8),
+        (2021, 9, 9),
+        (2021, 10, 10),
+        (2021, 11, 11),
+        (2021, 12, 12),
+        (2021, 14, 14),
+        (2021, 15, 15),
+        (2021, 17, 17),
+        (2021, 18, 18),
+        (2021, 19, 19),
+        (2021, 20, 20),
+        (2021, 21, 21),
+        (2021, 22, 22),
+        (2021, 23, 23),
+        (2021, 24, 24),
+        (2021, 34, 34),
+        (2021, 38, 38),
+        (2021, 41, 41),
+        (2021, 42, 42),
+        (2021, 43, 43),
+        (2021, 47, 47),
+        (2021, 48, 48),
+        (2021, 51, 51),
+        (2021, 77, 77),
+        (2021, 78, 78),
+        (2021, 99, 99),
+        (2022, 1, 1),
+        (2022, 2, 2),
+        (2022, 3, 3),
+        (2022, 4, 4),
+        (2022, 5, 5),
+        (2022, 6, 6),
+        (2022, 7, 7),
+        (2022, 8, 8),
+        (2022, 9, 9),
+        (2022, 10, 10),
+        (2022, 11, 11),
+        (2022, 12, 12),
+        (2022, 14, 14),
+        (2022, 15, 15),
+        (2022, 16, 16),
+        (2022, 17, 17),
+        (2022, 19, 19),
+        (2022, 20, 20),
+        (2022, 21, 21),
+        (2022, 22, 22),
+        (2022, 23, 23),
+        (2022, 24, 24),
+        (2022, 31, 31),
+        (2022, 34, 34),
+        (2022, 38, 38),
+        (2022, 41, 41),
+        (2022, 42, 42),
+        (2022, 43, 43),
+        (2022, 45, 45),
+        (2022, 47, 47),
+        (2022, 48, 48),
+        (2022, 51, 51),
+        (2022, 18, 54),
+        (2022, 77, 77),
+        (2022, 78, 78),
+        (2022, 99, 99))
 types = {"Daytona": "ss",
         "Talladega": "ss",
         "Atlanta": "ss",
@@ -52,6 +84,7 @@ types = {"Daytona": "ss",
         "Road America": "rc",
         "Indy Road Course": "rc",
         "Chicago Street": "rc",
+        "Daytona Road Course": "rc",
         "Richmond": "s",
         "Phoenix": "s",
         "Martinsville": "s",
@@ -79,7 +112,7 @@ testing = {}
 training = []
 
 print("******************************")
-print("retrieving data", end=" ", flush=True)
+print("training", end=" ")
 
 for numset in nums:
 
@@ -89,13 +122,14 @@ for numset in nums:
     sfins = []
     print(".", end=" ", flush=True)
 
-    for ind in range(len(yrs)):
+    for ind in (1, 2):
         num = numset[ind]
-        yr = yrs[ind]
+        yr = numset[0] + ind - 1
         url = "https://www.driveraverages.com/nascar/numberyear.php?carno_id=" + str(num) + "&yr_id=" + str(yr)
 
         # Send an HTTP GET request to the URL
         response = requests.get(url)
+
 
 
         soup = BeautifulSoup(response.content, "html.parser")
@@ -118,20 +152,23 @@ for numset in nums:
                     races = len(finishes)
                     finish = int(cells[6].get_text(strip=True))
 
-                    if ind == len(yrs) - 1:
+                    if ind == 2:
                         q = np.percentile(finishes, 25)
                         m = np.median(finishes)
                         start = int(cells[7].get_text(strip=True))
                         if type == "ss":
                             tq = np.percentile(ssfins, 25)
+                            tm = np.median(ssfins)
                             ss = 1
                         elif type == "rc":
                             tq = np.percentile(rcfins, 25)
+                            tm = np.median(rcfins)
                             ss = 0
                         else:
                             tq = np.percentile(sfins, 25)
+                            tm = np.median(sfins)
                             ss = 0
-                        training.append([start, q, m, tq, ss, finish])
+                        training.append([start, q, m, tq, tm, ss, finish])
 
 
                     finishes.append(finish)
@@ -142,15 +179,18 @@ for numset in nums:
                     else:
                         sfins.append(finish)
 
-                    if ind == len(yrs) - 1:
+                    if yr == 2023:
                         if num not in testing:
-                            testing[num] = [None for i in range(5)]
+                            testing[num] = [None for i in range(8)]
                         driverfile = testing[num]
                         driverfile[0] = np.percentile(finishes, 25)
                         driverfile[1] = np.median(finishes)
                         driverfile[2] = np.percentile(ssfins, 25)
-                        driverfile[3] = np.percentile(rcfins, 25)
-                        driverfile[4] = np.percentile(sfins, 25)
+                        driverfile[3] = np.median(ssfins)
+                        driverfile[4] = np.percentile(rcfins, 25)
+                        driverfile[5] = np.median(rcfins)
+                        driverfile[6] = np.percentile(sfins, 25)
+                        driverfile[7] = np.median(sfins)
 
 # Splitting data into inputs and results
 inputs = np.array(training)[:, :-1]
@@ -177,19 +217,22 @@ while True:
     qm = driverfile[1]
     if qtype == "ss":
         qtq = driverfile[2]
+        qtm = driverfile[3]
         ss = 1
     if qtype == "rc":
-        qtq = driverfile[3]
+        qtq = driverfile[4]
+        qtm = driverfile[5]
         ss = 0
     if qtype == "s":
-        qtq = driverfile[4]
+        qtq = driverfile[6]
+        qtm = driverfile[7]
         ss = 0
 
 
     # New point to classify
-    new_point = np.array([[qstart, qq, qm, qtq, ss]])
+    new_point = np.array([[qstart, qq, qm, qtq, qtm, ss]])
 
-    thrs = [str(i+1) for i in range(30)]
+    thrs = [1, 3, 5, 10, 15, 20, 25]
 
     probs = []
 
@@ -216,7 +259,7 @@ while True:
 
     dlt = [probs[0]]
     for i in range(len(probs)-1):
-        dlt.append(probs[i+1]-probs[i])
+        dlt.append((probs[i+1]-probs[i])/(thrs[i+1]-thrs[i]))
 
     norm = colors.Normalize(vmin=1/72, vmax=1/18)
 
@@ -228,15 +271,12 @@ while True:
         else:
             return 0
 
-    plt.figure(figsize=(10, 4))
 
     plt.style.use('dark_background')
-
-    spectrum = ['#0000FF', '#00FFFF', '#00FF00', '#FFFF00', '#FF0000']
-    colormap = colors.LinearSegmentedColormap.from_list('custom_colormap', spectrum, N=256)
-
-    # Create a bar graph with colored bars
-    bars = plt.bar(thrs, probs, color=plt.cm.viridis(norm(dlt)))
+    xpos = np.arange(len(thrs))
+    ypos = np.array(probs)/probs[-1]
+    bars = plt.bar(xpos, ypos, color=plt.cm.viridis(norm(dlt)))
+    plt.xticks(xpos, thrs)
 
 
     # Annotate each bar with its scaled value
@@ -248,39 +288,41 @@ while True:
         else:
             annotation = f'{value*100:.0f}'
         plt.text(bar.get_x() + bar.get_width() / 2, bar.get_height() + 0.01, annotation,
-                ha='center', va='bottom', color='white', size=7, font="arial")
+            ha='center', va='bottom', color='white', font="arial", size=18)
 
     ax = plt.gca()
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
     ax.spines['left'].set_visible(False)
-    ax.yaxis.set_visible(False)  # Hide the y-axis
+    ax.yaxis.set_visible(False)
+    ax.spines['bottom'].set_linewidth(2)
+    ax.tick_params(axis='x', width=2, length=6)
+    plt.xticks(fontname='arial', fontsize=18)
 
-    plt.xticks(fontname='Arial', fontsize=10)
+    image = "graph.png"
 
-    plt.text(0, 0.72, '#', color='white', fontsize=10,
-         ha='left', va='center', font='arial')
+    graphsize = 1548
+    numsize = 576
 
-    plt.text(0.4, 0.72, qnum, color='white', fontsize=30,
-            ha='left', va='center', font='arial', weight='bold')
+    blank = Image.new("RGB", (graphsize, graphsize), (0,0,0))
+    blank.save(image)
 
-    plt.text(0, 0.65, 'Track: ' + track, color='white', fontsize=7,
-            ha='left', va='center', font='arial')
+    plt.savefig(image, dpi=300, bbox_inches='tight')
 
-    plt.text(0, 0.62, 'Type: ' + qtype, color='white', fontsize=7,
-            ha='left', va='center', font='arial')
+    plt.clf()
 
-    plt.text(0, 0.59, 'Start: ' + str(qstart), color='white', fontsize=7,
-            ha='left', va='center', font='arial')
+    graph = Image.open(image)
 
-    plt.text(0, 0.56, 'Q: ' + str(round(qq, 0)), color='white', fontsize=7,
-            ha='left', va='center', font='arial')
+    numimage = Image.open(str(qnum) + ".jpg")
 
-    plt.text(0, 0.53, 'M: ' + str(round(qm, 0)), color='white', fontsize=7,
-            ha='left', va='center', font='arial')
+    # Create a new blank image with the same dimensions as the input images
+    resized = Image.new("RGB", (graphsize, graphsize), (0,0,0))
 
-    plt.text(0, 0.5, 'TQ: ' + str(round(qtq, 0)), color='white', fontsize=7,
-            ha='left', va='center', font='arial')
+    # Paste the first image onto the new image
+    resized.paste(graph, (0, 150))
 
+    # Paste the second image on top of the first image
+    resized.paste(numimage.resize((numsize, numsize)), (89, 0))
 
-    plt.savefig('chart.png', dpi=300, bbox_inches='tight')
+    # Save the final image
+    resized.save(image)

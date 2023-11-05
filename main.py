de1862cbe3aebb589421cf5e95268916e2e9a530
Tyler Longo <tylerjlongo9@gmail.com>
2023-11-05 14:50:21 -0500

    significantly improved graph generating efficiency

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import math
 from bs4 import BeautifulSoup
 import numpy as np
 from logreg import LogisticRegression
@@ -6,74 +7,73 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 from PIL import Image
 
-nums = ((2021, 2, 2),
-        (2021, 3, 3),
-        (2021, 4, 4),
-        (2021, 5, 5),
-        (2021, 6, 6),
-        (2021, 7, 7),
-        (2021, 8, 8),
-        (2021, 9, 9),
-        (2021, 10, 10),
-        (2021, 11, 11),
-        (2021, 12, 12),
-        (2021, 14, 14),
-        (2021, 15, 15),
-        (2021, 17, 17),
-        (2021, 18, 18),
-        (2021, 19, 19),
-        (2021, 20, 20),
-        (2021, 21, 21),
-        (2021, 22, 22),
-        (2021, 23, 23),
-        (2021, 24, 24),
-        (2021, 34, 34),
-        (2021, 38, 38),
-        (2021, 41, 41),
-        (2021, 42, 42),
-        (2021, 43, 43),
-        (2021, 47, 47),
-        (2021, 48, 48),
-        (2021, 51, 51),
-        (2021, 77, 77),
-        (2021, 78, 78),
-        (2021, 99, 99),
-        (2022, 1, 1),
-        (2022, 2, 2),
-        (2022, 3, 3),
-        (2022, 4, 4),
-        (2022, 5, 5),
-        (2022, 6, 6),
-        (2022, 7, 7),
-        (2022, 8, 8),
-        (2022, 9, 9),
-        (2022, 10, 10),
-        (2022, 11, 11),
-        (2022, 12, 12),
-        (2022, 14, 14),
-        (2022, 15, 15),
-        (2022, 16, 16),
-        (2022, 17, 17),
-        (2022, 19, 19),
-        (2022, 20, 20),
-        (2022, 21, 21),
-        (2022, 22, 22),
-        (2022, 23, 23),
-        (2022, 24, 24),
-        (2022, 31, 31),
-        (2022, 34, 34),
-        (2022, 38, 38),
-        (2022, 41, 41),
-        (2022, 42, 42),
-        (2022, 43, 43),
-        (2022, 45, 45),
-        (2022, 47, 47),
-        (2022, 48, 48),
-        (2022, 51, 51),
-        (2022, 18, 54),
-        (2022, 77, 77),
-        (2022, 78, 78),
-        (2022, 99, 99))
+nums = ((2021, '2', '2'),
+        (2021, '3', '3'),
+        (2021, '4', '4'),
+        (2021, '5', '5'),
+        (2021, '6', '6'),
+        (2021, '7', '7'),
+        (2021, '8', '8'),
+        (2021, '9', '9'),
+        (2021, '10', '10'),
+        (2021, '11', '11'),
+        (2021, '12', '12'),
+        (2021, '14', '14'),
+        (2021, '15', '15'),
+        (2021, '17', '17'),
+        (2021, '18', '18'),
+        (2021, '19', '19'),
+        (2021, '20', '20'),
+        (2021, '21', '21'),
+        (2021, '22', '22'),
+        (2021, '23', '23'),
+        (2021, '24', '24'),
+        (2021, '34', '34'),
+        (2021, '38', '38'),
+        (2021, '41', '41'),
+        (2021, '43', '43'),
+        (2021, '47', '47'),
+        (2021, '48', '48'),
+        (2021, '51', '51'),
+        (2021, '77', '77'),
+        (2021, '78', '78'),
+        (2021, '99', '99'),
+        (2022, '1', '1'),
+        (2022, '2', '2'),
+        (2022, '3', '3'),
+        (2022, '4', '4'),
+        (2022, '5', '5'),
+        (2022, '6', '6'),
+        (2022, '7', '7'),
+        (2022, '8', '8'),
+        (2022, '9', '9'),
+        (2022, '10', '10'),
+        (2022, '11', '11'),
+        (2022, '12', '12'),
+        (2022, '14', '14'),
+        (2022, '15', '15'),
+        (2022, '16', '16'),
+        (2022, '17', '17'),
+        (2022, '19', '19'),
+        (2022, '20', '20'),
+        (2022, '21', '21'),
+        (2022, '22', '22'),
+        (2022, '23', '23'),
+        (2022, '24', '24'),
+        (2022, '31', '31'),
+        (2022, '34', '34'),
+        (2022, '38', '38'),
+        (2022, '41', '41'),
+        (2022, '42', '42'),
+        (2022, '43', '43'),
+        (2022, '45', '45'),
+        (2022, '47', '47'),
+        (2022, '48', '48'),
+        (2022, '51', '51'),
+        (2022, '54', '18'),
+        (2022, '77', '77'),
+        (2022, '78', '78'),
+        (2022, '99', '99'))
 types = {"Daytona": "ss",
         "Talladega": "ss",
         "Atlanta": "ss",
@@ -104,7 +104,8 @@ types = {"Daytona": "ss",
         "Indianapolis": "s",
         "Nashville": "s",
         "Bristol Dirt": "s",
-        "Gateway (WWT)": "s"}
+        "Gateway (WWT)": "s",
+        "Kentucky": "s"}
 
 testing = {}
 
@@ -144,8 +145,12 @@ for numset in nums:
                 rownum += 1
             else:
                 cells = row.find_all(["th", "td"])
-                index = cells[3].get_text(strip=True)
-                if not index == "":
+                week = cells[3].get_text(strip=True)
+                if not week == "":
+                    week = int(week)
+                    uweek = math.ceil(week/6)
+                    regweek = math.ceil(week/1.5)
+
                     track = cells[5].get_text(strip=True)
                     type = types[track]
 
@@ -153,22 +158,47 @@ for numset in nums:
                     finish = int(cells[6].get_text(strip=True))
 
                     if ind == 2:
-                        q = np.percentile(finishes, 25)
-                        m = np.median(finishes)
                         start = int(cells[7].get_text(strip=True))
+
+                        last = np.percentile(finishes[:-week], 25)
+                        cur = np.percentile(finishes[-week:], 25)
+                        ovrq = cur * week/36 + last * (36-week)/36
+
+                        last = np.median(finishes[:-week])
+                        cur = np.median(finishes[-week:])
+                        ovrm = cur * week/36 + last * (36-week)/36
+
                         if type == "ss":
-                            tq = np.percentile(ssfins, 25)
-                            tm = np.median(ssfins)
+                            last = np.percentile(ssfins[:-uweek], 25)
+                            cur = np.percentile(ssfins[-uweek:], 25)
+                            tq = cur * week/36 + last * (36-week)/36
+
+                            last = np.median(ssfins[:-uweek])
+                            cur = np.median(ssfins[-uweek:])
+                            tm = cur * week/36 + last * (36-week)/36
+
                             ss = 1
                         elif type == "rc":
-                            tq = np.percentile(rcfins, 25)
-                            tm = np.median(rcfins)
+                            last = np.percentile(rcfins[:-uweek], 25)
+                            cur = np.percentile(rcfins[-uweek:], 25)
+                            tq = cur * week/36 + last * (36-week)/36
+
+                            last = np.median(rcfins[:-uweek])
+                            cur = np.median(rcfins[-uweek:])
+                            tm = cur * week/36 + last * (36-week)/36
+
                             ss = 0
                         else:
-                            tq = np.percentile(sfins, 25)
-                            tm = np.median(sfins)
+                            last = np.percentile(sfins[:-regweek], 25)
+                            cur = np.percentile(sfins[-regweek:], 25)
+                            tq = cur * week/36 + last * (36-week)/36
+
+                            last = np.median(sfins[:-regweek])
+                            cur = np.median(sfins[-regweek:])
+                            tm = cur * week/36 + last * (36-week)/36
+
                             ss = 0
-                        training.append([start, q, m, tq, tm, ss, finish])
+                        training.append([start, ovrq, ovrm, tq, tm, ss, finish])
 
 
                     finishes.append(finish)
@@ -183,14 +213,38 @@ for numset in nums:
                         if num not in testing:
                             testing[num] = [None for i in range(8)]
                         driverfile = testing[num]
-                        driverfile[0] = np.percentile(finishes, 25)
-                        driverfile[1] = np.median(finishes)
-                        driverfile[2] = np.percentile(ssfins, 25)
-                        driverfile[3] = np.median(ssfins)
-                        driverfile[4] = np.percentile(rcfins, 25)
-                        driverfile[5] = np.median(rcfins)
-                        driverfile[6] = np.percentile(sfins, 25)
-                        driverfile[7] = np.median(sfins)
+
+                        last = np.percentile(finishes[:-week], 25)
+                        cur = np.percentile(finishes[-week:], 25)
+                        driverfile[0] = cur * week/36 + last * (36-week)/36
+
+                        last = np.median(finishes[:-week])
+                        cur = np.median(finishes[-week:])
+                        driverfile[1] = cur * week/36 + last * (36-week)/36
+
+                        last = np.percentile(ssfins[:-uweek], 25)
+                        cur = np.percentile(ssfins[-uweek:], 25)
+                        driverfile[2] = cur * week/36 + last * (36-week)/36
+
+                        last = np.median(ssfins[:-uweek])
+                        cur = np.median(ssfins[-uweek:])
+                        driverfile[3] = cur * week/36 + last * (36-week)/36
+
+                        last = np.percentile(rcfins[:-uweek], 25)
+                        cur = np.percentile(rcfins[-uweek:], 25)
+                        driverfile[4] = cur * week/36 + last * (36-week)/36
+
+                        last = np.median(rcfins[:-uweek])
+                        cur = np.median(rcfins[-uweek:])
+                        driverfile[5] = cur * week/36 + last * (36-week)/36
+
+                        last = np.percentile(sfins[:-regweek], 25)
+                        cur = np.percentile(sfins[-regweek:], 25)
+                        driverfile[6] = cur * week/36 + last * (36-week)/36
+
+                        last = np.median(sfins[:-regweek])
+                        cur = np.median(sfins[-regweek:])
+                        driverfile[7] = cur * week/36 + last * (36-week)/36
 
 # Splitting data into inputs and results
 inputs = np.array(training)[:, :-1]
@@ -198,11 +252,34 @@ results = np.array(training)[:, -1]
 
 print("")
 
+
+
+thrs = [1, 3, 5, 10, 15, 20, 25]
+weights = []
+
+for thr in thrs:
+        outputs = []
+        for result in results:
+            if result > int(thr):
+                outputs.append(0)
+            else:
+                outputs.append(1)
+
+
+        # Creating and training the Logistic Regression model
+        model = LogisticRegression()
+        model.fit(inputs, outputs)
+        weights.append(model.weights)
+
+print(weights)
+
+
+
 while True:
     print("******************************")
 
     print("Driver Number:", end=" ")
-    qnum = int(input())
+    qnum = input()
 
     print("Starting Position:", end=" ")
     qstart = int(input())
@@ -232,24 +309,11 @@ while True:
     # New point to classify
     new_point = np.array([[qstart, qq, qm, qtq, qtm, ss]])
 
-    thrs = [1, 3, 5, 10, 15, 20, 25]
-
     probs = []
 
-    for thr in thrs:
-        outputs = []
-        for result in results:
-            if result > int(thr):
-                outputs.append(0)
-            else:
-                outputs.append(1)
-
-
-        # Creating and training the Logistic Regression model
-        model = LogisticRegression()
-        model.fit(inputs, outputs)
-
+    for i in range(len(thrs)):
         # Predicting the probability of each class for the new point
+        model.weights = weights[i]
         prediction = model.predict(new_point)
         probs.append(prediction[0])
     

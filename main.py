7646bf6ecf4b46977df2d272b29ae04081b7c893
Tyler Longo <tylerjlongo9@gmail.com>
2023-11-06 00:50:01 -0500

    read testing/training data from txt files

--- main.py
+++ main.py
@@ -1,261 +1,40 @@
-import requests
-import math
-from bs4 import BeautifulSoup
 import numpy as np
 from logreg import LogisticRegression
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 from PIL import Image
 
-nums = ((2021, '2', '2'),
-        (2021, '3', '3'),
-        (2021, '4', '4'),
-        (2021, '5', '5'),
-        (2021, '6', '6'),
-        (2021, '7', '7'),
-        (2021, '8', '8'),
-        (2021, '9', '9'),
-        (2021, '10', '10'),
-        (2021, '11', '11'),
-        (2021, '12', '12'),
-        (2021, '14', '14'),
-        (2021, '15', '15'),
-        (2021, '17', '17'),
-        (2021, '18', '18'),
-        (2021, '19', '19'),
-        (2021, '20', '20'),
-        (2021, '21', '21'),
-        (2021, '22', '22'),
-        (2021, '23', '23'),
-        (2021, '24', '24'),
-        (2021, '34', '34'),
-        (2021, '38', '38'),
-        (2021, '41', '41'),
-        (2021, '43', '43'),
-        (2021, '47', '47'),
-        (2021, '48', '48'),
-        (2021, '51', '51'),
-        (2021, '77', '77'),
-        (2021, '78', '78'),
-        (2021, '99', '99'),
-        (2022, '1', '1'),
-        (2022, '2', '2'),
-        (2022, '3', '3'),
-        (2022, '4', '4'),
-        (2022, '5', '5'),
-        (2022, '6', '6'),
-        (2022, '7', '7'),
-        (2022, '8', '8'),
-        (2022, '9', '9'),
-        (2022, '10', '10'),
-        (2022, '11', '11'),
-        (2022, '12', '12'),
-        (2022, '14', '14'),
-        (2022, '15', '15'),
-        (2022, '16', '16'),
-        (2022, '17', '17'),
-        (2022, '19', '19'),
-        (2022, '20', '20'),
-        (2022, '21', '21'),
-        (2022, '22', '22'),
-        (2022, '23', '23'),
-        (2022, '24', '24'),
-        (2022, '31', '31'),
-        (2022, '34', '34'),
-        (2022, '38', '38'),
-        (2022, '41', '41'),
-        (2022, '42', '42'),
-        (2022, '43', '43'),
-        (2022, '45', '45'),
-        (2022, '47', '47'),
-        (2022, '48', '48'),
-        (2022, '51', '51'),
-        (2022, '54', '18'),
-        (2022, '77', '77'),
-        (2022, '78', '78'),
-        (2022, '99', '99'))
-types = {"Daytona": "ss",
-        "Talladega": "ss",
-        "Atlanta": "ss",
-        "Sonoma": "rc",
-        "Watkins Glen": "rc",
-        "Charlotte Roval": "rc",
-        "COTA": "rc",
-        "Road America": "rc",
-        "Indy Road Course": "rc",
-        "Chicago Street": "rc",
-        "Daytona Road Course": "rc",
-        "Richmond": "s",
-        "Phoenix": "s",
-        "Martinsville": "s",
-        "Charlotte": "s",
-        "Texas": "s",
-        "Kansas": "s",
-        "Dover": "s",
-        "Bristol": "s",
-        "New Hampshire": "s",
-        "Pocono": "s",
-        "Las Vegas": "s",
-        "Michigan": "s",
-        "Atlanta": "s",
-        "Darlington": "s",
-        "California (Auto Club)": "s",
-        "Homestead": "s",
-        "Indianapolis": "s",
-        "Nashville": "s",
-        "Bristol Dirt": "s",
-        "Gateway (WWT)": "s",
-        "Kentucky": "s"}
-
-testing = {}
-
-# Initialize a 2D list to store the table data
 training = []
 
-print("******************************")
-print("training", end=" ")
-
-for numset in nums:
-
-    finishes = []
-    ssfins = []
-    rcfins = []
-    sfins = []
-    print(".", end=" ", flush=True)
-
-    for ind in (1, 2):
-        num = numset[ind]
-        yr = numset[0] + ind - 1
-        url = "https://www.driveraverages.com/nascar/numberyear.php?carno_id=" + str(num) + "&yr_id=" + str(yr)
-
-        # Send an HTTP GET request to the URL
-        response = requests.get(url)
-
-
-
-        soup = BeautifulSoup(response.content, "html.parser")
-            
-        # Find the table containing the data
-        table = soup.find("table")
-
-        # Loop through each row in the table
-        rownum = 0
-        for row in table.find_all("tr"):
-            if rownum < 5:
-                rownum += 1
-            else:
-                cells = row.find_all(["th", "td"])
-                week = cells[3].get_text(strip=True)
-                if not week == "":
-                    week = int(week)
-                    uweek = math.ceil(week/6)
-                    regweek = math.ceil(week/1.5)
-
-                    track = cells[5].get_text(strip=True)
-                    type = types[track]
-
-                    races = len(finishes)
-                    finish = int(cells[6].get_text(strip=True))
-
-                    if ind == 2:
-                        start = int(cells[7].get_text(strip=True))
-
-                        last = np.percentile(finishes[:-week], 25)
-                        cur = np.percentile(finishes[-week:], 25)
-                        ovrq = cur * week/36 + last * (36-week)/36
-
-                        last = np.median(finishes[:-week])
-                        cur = np.median(finishes[-week:])
-                        ovrm = cur * week/36 + last * (36-week)/36
-
-                        if type == "ss":
-                            last = np.percentile(ssfins[:-uweek], 25)
-                            cur = np.percentile(ssfins[-uweek:], 25)
-                            tq = cur * week/36 + last * (36-week)/36
-
-                            last = np.median(ssfins[:-uweek])
-                            cur = np.median(ssfins[-uweek:])
-                            tm = cur * week/36 + last * (36-week)/36
-
-                            ss = 1
-                        elif type == "rc":
-                            last = np.percentile(rcfins[:-uweek], 25)
-                            cur = np.percentile(rcfins[-uweek:], 25)
-                            tq = cur * week/36 + last * (36-week)/36
-
-                            last = np.median(rcfins[:-uweek])
-                            cur = np.median(rcfins[-uweek:])
-                            tm = cur * week/36 + last * (36-week)/36
-
-                            ss = 0
-                        else:
-                            last = np.percentile(sfins[:-regweek], 25)
-                            cur = np.percentile(sfins[-regweek:], 25)
-                            tq = cur * week/36 + last * (36-week)/36
-
-                            last = np.median(sfins[:-regweek])
-                            cur = np.median(sfins[-regweek:])
-                            tm = cur * week/36 + last * (36-week)/36
-
-                            ss = 0
-                        training.append([start, ovrq, ovrm, tq, tm, ss, finish])
-
-
-                    finishes.append(finish)
-                    if type == "ss":
-                        ssfins.append(finish)
-                    elif type == "rc":
-                        rcfins.append(finish)
-                    else:
-                        sfins.append(finish)
-
-                    if yr == 2023:
-                        if num not in testing:
-                            testing[num] = [None for i in range(8)]
-                        driverfile = testing[num]
-
-                        last = np.percentile(finishes[:-week], 25)
-                        cur = np.percentile(finishes[-week:], 25)
-                        driverfile[0] = cur * week/36 + last * (36-week)/36
-
-                        last = np.median(finishes[:-week])
-                        cur = np.median(finishes[-week:])
-                        driverfile[1] = cur * week/36 + last * (36-week)/36
-
-                        last = np.percentile(ssfins[:-uweek], 25)
-                        cur = np.percentile(ssfins[-uweek:], 25)
-                        driverfile[2] = cur * week/36 + last * (36-week)/36
-
-                        last = np.median(ssfins[:-uweek])
-                        cur = np.median(ssfins[-uweek:])
-                        driverfile[3] = cur * week/36 + last * (36-week)/36
-
-                        last = np.percentile(rcfins[:-uweek], 25)
-                        cur = np.percentile(rcfins[-uweek:], 25)
-                        driverfile[4] = cur * week/36 + last * (36-week)/36
-
-                        last = np.median(rcfins[:-uweek])
-                        cur = np.median(rcfins[-uweek:])
-                        driverfile[5] = cur * week/36 + last * (36-week)/36
-
-                        last = np.percentile(sfins[:-regweek], 25)
-                        cur = np.percentile(sfins[-regweek:], 25)
-                        driverfile[6] = cur * week/36 + last * (36-week)/36
-
-                        last = np.median(sfins[:-regweek])
-                        cur = np.median(sfins[-regweek:])
-                        driverfile[7] = cur * week/36 + last * (36-week)/36
+# Read data from the text file and convert it to a list of lists
+with open('training.txt', 'r') as file:
+    lines = file.readlines()
+    # Parse the lines into a list of lists
+    for line in lines:
+        # Split the line into individual values using comma as the separator
+        values = line.strip().split(', ')[4:]
+        # Convert values to integers and append them to the data list
+        training.append([float(value) for value in values])
 
 # Splitting data into inputs and results
 inputs = np.array(training)[:, :-1]
 results = np.array(training)[:, -1]
 
-print("")
 
+testing = {}
+
+with open('testing.txt', 'r') as file:
+    lines = file.readlines()
+    for line in lines:
+        # Split the line into key and values
+        key, values = line.strip().split(': ')
+        # Convert values to a list of integers
+        testing[key] = list(map(float, values.split(', ')))
 
 
 thrs = [1, 3, 5, 10, 15, 20, 25]
 weights = []
+bias = []
 
 for thr in thrs:
         outputs = []
@@ -270,6 +49,7 @@ for thr in thrs:
         model = LogisticRegression()
         model.fit(inputs, outputs)
         weights.append(model.weights)
+        bias.append(model.bias)
 
 print(weights)
 
@@ -281,12 +61,8 @@ while True:
     print("Driver Number:", end=" ")
     qnum = input()
 
-    print("Starting Position:", end=" ")
-    qstart = int(input())
-
-    print("Track:", end=" ")
-    track = input()
-    qtype = types[track]
+    print("Track Type:", end=" ")
+    qtype = input()
 
     driverfile = testing[qnum]
 
@@ -305,15 +81,17 @@ while True:
         qtm = driverfile[7]
         ss = 0
 
+    print([qq, qm, qtq, qtm, ss])
 
     # New point to classify
-    new_point = np.array([[qstart, qq, qm, qtq, qtm, ss]])
+    new_point = np.array([[qq, qm, qtq, qtm, ss, 36]])
 
     probs = []
 
     for i in range(len(thrs)):
         # Predicting the probability of each class for the new point
         model.weights = weights[i]
+        model.bias = bias[i]
         prediction = model.predict(new_point)
         probs.append(prediction[0])
     
